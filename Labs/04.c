e38a3e6ca8cdaf24efd685aa6436566067538881
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2023-09-12 22:53:48 +0500

    Update 04.c

--- Labs/04.c
+++ Labs/04.c
@@ -1,4 +1,4 @@
-Q1:
+Question1:
 
 #include<stdio.h>
 
@@ -20,7 +20,7 @@ int main() {
   }
 }
 
-Q2:
+Question2:
 
 #include<stdio.h>
 
@@ -65,4 +65,37 @@ int main() {
 	}
 	printf("The sum of numbers is:");
 	printf("%d",sum);
+}
+
+Question4:
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	//: DECLARING VARIABLES HERE
+	int a;
+	int b;
+	int c;
+	float X1;
+	float X2;
+	
+	//: TAKING INPUTS HERE
+	printf("Enter the coefficient of x^2:");
+	scanf("%d",&a);
+	printf("Enter the coeeficient of x:");
+	scanf("%d",&b);
+	printf("Enter the value of constant in the expression:");
+	scanf("%d",&c);
+	
+	//: DOING CALCULATION HERE
+	X1 = ((-1*b) + sqrt(b*b - 4*a*c))/(2*a);
+	X2 = ((-1*b) - sqrt(b*b - 4*a*c))/(2*a);
+	
+	//: PRINTING ANSWERS HERE
+	printf("The first root is equal to:");
+	printf("%f",X1);
+	printf("\n");
+	printf("The second root is equal to:");
+	printf("%f",X2);
 }
