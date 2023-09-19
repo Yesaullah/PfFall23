a248c51c588e3ebcadc775b13812296dcb0a428e
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2023-09-19 21:07:05 +0500

    Add files via upload

---
+++ Labs/05/P7.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+	
+	int OrigPrice;
+	int save;
+	int salP;
+	float disc;
+	
+	printf("Enter the original price of the item: \n ");
+	scanf("%d", &OrigPrice);
+	
+	if (OrigPrice >= 2000 && OrigPrice <= 4000) 
+	{
+		disc=0.2;
+		save=(OrigPrice)*(disc);
+		salP=OrigPrice-save;
+		printf("%d is the amount saved. \n", save);
+		printf("%d is the discounted price.", salP);
+	}
+	else if(OrigPrice >= 4001 && OrigPrice <= 6000)
+	{
+		disc=0.3;
+		save=(OrigPrice)*(disc);
+		salP=OrigPrice - save;
+		printf("%d is the amount saved. \n", save);
+		printf("%d is the discounted price.", salP);
+	}
+	else if (OrigPrice > 6000)
+	{
+		disc=0.5;
+		save=(OrigPrice)*(disc);
+		salP=OrigPrice-save;
+		printf("%d is the amount saved. \n", save);
+		printf("%d is the discounted price.", salP);
+	}
+	else 
+	{
+		printf("There is no discount on this price.");
+	}
+	
+}
