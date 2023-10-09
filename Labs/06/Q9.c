46c184f2737707bacb029ea12ab1e3130a472d5d
Yesaullah <142867724+Yesaullah@users.noreply.github.com>
2023-10-09 19:08:51 +0500

    Add files via upload

---
+++ Labs/06/Q9.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	int i;
+	printf("Enter the number of elements in the array: ");
+	scanf("%d", &n);
+	int arr[n];
+	
+	printf("Enter %d elements in the array:\n", n);
+	for(i=0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	
+	int min = arr[0];
+	int max = arr[0];
+	
+	for(i = 1; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+	}
+	
+	printf("Minimum value: %d\n", min);
+    printf("Maximum value: %d\n", max);
+
+}
